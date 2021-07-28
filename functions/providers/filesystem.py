4c38af567714b2e550a1853cce1e14191f6d092b
Chris Thorpe <drchristhorpe@googlemail.com>
2021-07-28 14:41:43 +0100

    Cleaned up the filesystem provider

--- functions/providers/filesystem.py
+++ functions/providers/filesystem.py
@@ -2,26 +2,51 @@ import logging
 import json
 
 
-def filesystem():
-    return True
-
-
-
 class filesystemProvider():
 
     basedir = ''
 
     def __init__(self,basedir):
-        self.results = None
-        self.errors = []
         self.basedir = basedir
 
-    def __buildfilepath(self, filename, format):
+    def __build_filepath(self, filename, format):
         filepath = self.basedir +'/' + filename + '.' + format
         return filepath
     
-    def get(self,filename,format='json'):
-        logging.warn(self.__buildfilepath(filename,format))
-        return True
+    def get_file_handle(self, filename, format, mode):
+        return open(self.__build_filepath(filename, format), mode)
 
+
+    def get(self,filename,format='json'):
+        errors = []
+        success = True
+        data = None
+        _file = None
+        try:
+            _file = self.get_file_handle(filename, format, 'r')
+        except:
+            errors.append('not_found')
+        if _file:
+            if format == 'json':
+                data = json.load(_file)
+                logging.warn(data)
+            else:
+                data = _file.read()
+        return data, success, errors
+
+
+    def put(self, filename , payload, format='json'):
+        errors = []
+        success = True
+        data = None
+        _file = None
+        try:
+            _file = self.get_file_handle(filename, format, 'r')
+        except:
+            errors.append('not_found')
+        if _file:
+            _file.write(payload)
+            _file.close()     
+            data = payload   
+        return data, success, errors
 

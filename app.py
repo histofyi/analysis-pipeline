7c62eeac6d05e5d810a3585dfefd20fcd18bf0bf
Chris Thorpe <drchristhorpe@googlemail.com>
2021-07-29 15:21:35 +0100

    WIP getting and displaying information about the structure

--- app.py
+++ app.py
@@ -13,6 +13,8 @@ import functions
 
 from api import api
 
+import ssl
+
 
 
 app = Flask(__name__)
@@ -35,11 +37,42 @@ def home_handler():
 @app.get('/structures/information/<path:pdb_code>')
 def structure_info_handler(pdb_code):
     rcsb = pdb.RCSB()
+
     pdb_info = rcsb.get_info(pdb_code)
-    pdb_info_text = json.dumps(pdb_info, sort_keys=True, indent=4)
+
     pdb_file = rcsb.fetch(pdb_code)
+
     pdb_image_folder = pdb_code[1:3]
-    return template.render('structure_info', {'pdb_file':pdb_file, 'pdb_code':pdb_code, 'pdb_info':pdb_info, 'pdb_info_text':pdb_info_text, 'pdb_image_folder':pdb_image_folder})
+
+    structure = rcsb.load_structure(pdb_code)
+
+    try:
+        doi_url = rcsb.resolve_doi(pdb_info["rcsb_primary_citation"]["pdbx_database_id_doi"])
+    except:
+        doi_url = None
+    
+    chains = [chain.id for chain in structure.get_chains()]
+
+    chain_sequences = [[residue.resname for residue in chain if residue.resname != 'HOH'] for chain in structure.get_chains()]
+
+
+    logging.warn(chain_sequences)
+
+    structure_stats = {
+        'chains': chains,
+        'chain_count': len(chains)
+    }
+
+    variables = {
+        'pdb_file':pdb_file, 
+        'pdb_code':pdb_code, 
+        'pdb_info':pdb_info, 
+        'pdb_info_text':json.dumps(pdb_info, sort_keys=True, indent=4), 
+        'pdb_image_folder':pdb_image_folder, 
+        'doi_url':doi_url,
+        'structure_stats':structure_stats
+    }
+    return template.render('structure_info', variables)
 
 
 

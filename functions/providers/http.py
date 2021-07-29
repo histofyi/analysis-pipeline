09a898fc3419723fda48b29309bd05ce6d9c659a
Chris Thorpe <33100149+drchristhorpe@users.noreply.github.com>
2021-07-29 13:18:35 +0100

    Handling json better

--- functions/providers/http.py
+++ functions/providers/http.py
@@ -10,7 +10,10 @@ class httpProvider():
     def get(self, url, format, params=None):
         r = requests.get(url)
         if r.status_code == 200:
-            content = r.text
+            if format == 'txt':
+                content = r.text
+            elif format == 'json':
+                content = r.json()
             return content
         else:
             return None

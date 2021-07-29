106d1864ca8fb4a219b9b35312ba2af95a4471ea
Chris Thorpe <33100149+drchristhorpe@users.noreply.github.com>
2021-07-29 13:22:55 +0100

    Caching of data in a 'get or create' pattern

--- functions/pdb/rcsb.py
+++ functions/pdb/rcsb.py
@@ -1,14 +1,26 @@
 import json
+import toml
+import logging
+
+
 
 from ..providers import httpProvider
+from ..providers import filesystemProvider
+
+
 
 http = httpProvider()
+file = filesystemProvider(None)
 
 class RCSB():
 
     def fetch(self, pdb_code):
-        url = 'https://files.rcsb.org/download/'+ pdb_code +'.pdb'
-        pdb_data = http.get(url, 'txt')
+        filepath = 'structures/pdb_format/raw/{pdb_code}'.format(pdb_code = pdb_code)
+        pdb_data, success, errors = file.get(filepath, format='pdb')
+        if not success:
+            url = 'https://files.rcsb.org/download/{pdb_code}.pdb'.format(pdb_code = pdb_code)
+            pdb_data = http.get(url, 'txt')
+            payload, success, errors = file.put(filepath, pdb_data, format='pdb')
         return pdb_data
 
 
@@ -17,5 +29,15 @@ class RCSB():
         search_content = http.post(url, json.dumps(query), 'json')
         pdb_data = [entry['identifier'].lower() for entry in search_content['result_set']]
         return pdb_data
+
+
+    def get_info(self, pdb_code):
+        filepath = 'structures/pdb_info/{pdb_code}'.format(pdb_code = pdb_code)
+        pdb_info, success, errors = file.get(filepath)
+        if not success:
+            url = 'https://data.rcsb.org/rest/v1/core/entry/{pdb_code}'.format(pdb_code = pdb_code)
+            pdb_info = http.get(url, 'json')
+            payload, success, errors = file.put(filepath, json.dumps(pdb_info))
+        return pdb_info
 
 

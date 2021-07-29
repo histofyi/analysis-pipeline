d16390b57ede770f3a6d7b44bf8eed74b94257d7
Chris Thorpe <33100149+drchristhorpe@users.noreply.github.com>
2021-07-29 13:23:57 +0100

    Getting information from PDB as well as file. Using the filesystem to get a welcome message

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request
 import toml
+import json
 import logging
 
 
@@ -26,15 +27,18 @@ molecules, success, errors = filesystem.get('constants/shared/molecules')
 
 @app.get('/')
 def home_handler():
-    pdb_file = pdb.RCSB().fetch('2hla')
-    return template.render('index', {'pdb_file':pdb_file})
+    scratch_json, success, errors = filesystem.get('scratch/hello')
+    return template.render('index', {'pdb_file':scratch_json})
 
 
 
 @app.get('/structures/information/<path:pdb_code>')
 def structure_info_handler(pdb_code):
-    pdb_file = pdb.RCSB().fetch(pdb_code)
-    return template.render('structure_info', {'pdb_file':pdb_file,'pdb_code':pdb_code})
+    rcsb = pdb.RCSB()
+    pdb_info = rcsb.get_info(pdb_code)
+    pdb_info_text = json.dumps(pdb_info, sort_keys=True, indent=4)
+    pdb_file = rcsb.fetch(pdb_code)
+    return template.render('structure_info', {'pdb_file':pdb_file, 'pdb_code':pdb_code, 'pdb_info':pdb_info, 'pdb_info_text':pdb_info_text})
 
 
 
@@ -42,9 +46,7 @@ def structure_info_handler(pdb_code):
 @app.get('/structures/search/<path:mhc_class>')
 def structures_search_handler(mhc_class):
     query_name = mhc_class + '_sequence_query'
-    logging.warn(query_name)
     query, success, errors = filesystem.get('constants/rcsb/'+ query_name)
-    logging.warn(query)
     search_data = pdb_data = pdb.RCSB().search(query)
     return template.render('structure_search', {'search_data':search_data,'molecule_metadata':molecules[mhc_class],'count':len(search_data)})
 

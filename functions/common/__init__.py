8857d8f30afb5c3d4cfb72f36bbd809f8febd01a
Chris Thorpe <drchristhorpe@googlemail.com>
2021-08-01 13:58:20 +0100

    Better handling of form elements and losing empty variables

--- functions/common/__init__.py
+++ functions/common/__init__.py
@@ -6,11 +6,13 @@ import logging
 def request_variables(params, ignore_empty_strings=True):
     if not params:
         params = str(request.form['params'])
+        logging.warn(request.form)
         if len(params) > 0:
             params = json.loads(params)
         else:
             params = []
     variables = {}
+    logging.warn(params)
     for param in params:
         if request.method == "GET":
             variables[param] = request.args.get(param)
@@ -25,9 +27,18 @@ def request_variables(params, ignore_empty_strings=True):
 
         if variables[param] is not None and len(variables[param]) == 0 and ignore_empty_strings is True:
             variables[param] = None
+        if variables[param] == 'none':
+            variables[param] = None
         if variables[param] == 'on':
             variables[param] = True
         if variables[param] == 'off':
             variables[param] = False
+    
+    if ignore_empty_strings is True:
+        clean_variables = {}
+        for param in params:
+            if variables[param] is not None:
+                clean_variables[param] = variables[param]
+        variables = clean_variables
 
     return variables

72ad9a102ec4516563a60a74a8762a2c0a957f7c
Chris Thorpe <33100149+drchristhorpe@users.noreply.github.com>
2021-08-13 12:09:49 +0100

    Improvements to the splitting and aligning functions

--- functions/structure/align_structures.py
+++ functions/structure/align_structures.py
@@ -11,9 +11,9 @@ def align_structure(mhc_class,pdb_code, complex_number, target_chain_id):
     rcsb = RCSB()
     baseline = rcsb.load_structure(mhc_class, directory = 'structures/pdb_format/orientations')
     complex_filename = '{pdb_code}_{complex_number}'.format(pdb_code = pdb_code, complex_number = complex_number)
-    try:
+    if complex_filename:
         target = rcsb.load_structure(complex_filename, directory = 'structures/pdb_format/single_assemblies')
-    except:
+    else:
         target = None
     
     if target is not None:
@@ -27,6 +27,10 @@ def align_structure(mhc_class,pdb_code, complex_number, target_chain_id):
         baseline_atoms = []
         target_atoms = []
 
+        baseline_res_ids = []
+        baseline_res_aa = []
+        target_res_ids = []
+        target_res_aa = []
 
         # Iterate of all chains in the model in order to find all residues
         for baseline_chain in baseline_model:
@@ -35,6 +39,8 @@ def align_structure(mhc_class,pdb_code, complex_number, target_chain_id):
                 for baseline_residues in baseline_chain:
                     # Check if residue number ( .get_id() ) is in the list
                     if baseline_residues.get_id()[1] in residues_to_be_aligned:
+                        baseline_res_ids.append(baseline_residues.get_id()[1])
+                        baseline_res_aa.append(baseline_residues.resname)
                         # Append CA atom to list
                         baseline_atoms.append(baseline_residues['CA'])
 
@@ -43,9 +49,20 @@ def align_structure(mhc_class,pdb_code, complex_number, target_chain_id):
         for target_chain in target_model:
             if target_chain.id == target_chain_id:
                 logging.warn(target_chain.id)
-                for target_res in target_chain:
-                    if target_res.get_id()[1] in residues_to_be_aligned:
-                        target_atoms.append(target_res['CA'])
+                for target_residues in target_chain:
+                    if target_residues.get_id()[1] in residues_to_be_aligned:
+                        target_res_ids.append(target_residues.get_id()[1])
+                        target_res_aa.append(target_residues.resname)
+                        target_atoms.append(target_residues['CA'])
+
+
+        logging.warn(len(baseline_atoms))
+        logging.warn(len(target_atoms))
+        logging.warn(baseline_res_ids)
+        logging.warn(target_res_ids)
+        logging.warn(baseline_res_aa)
+        logging.warn(target_res_aa)
+
 
         super_imposer = Bio.PDB.Superimposer()
         super_imposer.set_atoms(baseline_atoms, target_atoms)
--- functions/structure/split_assemblies.py
+++ functions/structure/split_assemblies.py
@@ -12,7 +12,7 @@ class SelectChains(PDB.Select):
 
 def split_assemblies(histo_info, current_assembly, pdb_code):
     split_information = None
-    if histo_info['best_match']['confidence'] > 0.9:
+    if histo_info['best_match']['confidence'] > 0.8:
         split_information = {}
         chains = histo_info['chain_assignments']
         assembly_count = histo_info['basic_info']['assembly_count']
@@ -25,19 +25,23 @@ def split_assemblies(histo_info, current_assembly, pdb_code):
                 this_complex.append(chains[chain]['chains'][i])
             complex_array.append(this_complex)
             i += 1
-    
-    j = 1
-    complexes = []
-    for complex in complex_array:
-        complex_filename = '{pdb_code}_{complex}.pdb'.format(pdb_code = pdb_code, complex = str(j))
-        complex_filepath = '../../data/structures/pdb_format/single_assemblies/{filename}'.format(filename = complex_filename)  
-        j += 1
-        writer = PDB.PDBIO()
-        writer.set_structure(current_assembly)
-        writer.save(complex_filepath, select=SelectChains(complex))
-        complexes.append({
-            'filename': complex_filename,
-            'chains': complex
-        })
-    split_information['complexes'] = complexes    
+    else:
+        complex_array = []
+    if len(complex_array) > 0:
+        j = 1
+        complexes = []
+        for complex in complex_array:
+            complex_filename = '{pdb_code}_{complex}.pdb'.format(pdb_code = pdb_code, complex = str(j))
+            complex_filepath = '../../data/structures/pdb_format/single_assemblies/{filename}'.format(filename = complex_filename)  
+            j += 1
+            writer = PDB.PDBIO()
+            writer.set_structure(current_assembly)
+            writer.save(complex_filepath, select=SelectChains(complex))
+            complexes.append({
+                'filename': complex_filename,
+                'chains': complex
+            })
+        split_information['complexes'] = complexes    
+    else:
+        split_information = None
     return split_information

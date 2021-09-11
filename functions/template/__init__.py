b7f4a5f418686433b121330c3c3a4535459bb20b
Chris Thorpe <33100149+drchristhorpe@users.noreply.github.com>
2021-09-11 20:25:06 +0100

    Added templating decorator

--- functions/template/__init__.py
+++ functions/template/__init__.py
@@ -1,7 +1,27 @@
-from flask import render_template
+from flask import request, render_template
+from functools import wraps
+
+import logging
 
 
 def render(template_name, variables):
     if ".html" not in template_name:
         template_name += ".html"
     return render_template(template_name, **variables)
+
+
+def templated(template=None):
+    def decorator(f):
+        @wraps(f)
+        def decorated_function(*args, **kwargs):
+            template_name = template
+            if template_name is None:
+                template_name = f"{request.endpoint.replace('.', '/')}.html"
+            ctx = f(*args, **kwargs)
+            if ctx is None:
+                ctx = {}
+            elif not isinstance(ctx, dict):
+                return ctx
+            return render(template_name, ctx)
+        return decorated_function
+    return decorator

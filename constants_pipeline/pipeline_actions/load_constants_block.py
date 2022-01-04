90ecdccdaf0417402899c11e0f0d3326072bcecf
Chris Thorpe <33100149+drchristhorpe@users.noreply.github.com>
2022-01-04 17:07:31 +0000

    Tidied up, removing logging

--- constants_pipeline/pipeline_actions/load_constants_block.py
+++ constants_pipeline/pipeline_actions/load_constants_block.py
@@ -6,17 +6,15 @@ from .common import build_s3_constants_key
 import logging
 
 fs = filesystemProvider('constants_pipeline/files')
+
 constants_files = ['amino_acids','chains','hetatoms','loci','peptide_lengths','species_overrides']
 
 
 def load_constants(aws_config):
     s3 = s3Provider(aws_config)
     for filename in constants_files:
-        logging.warn(filename)
         data, success, errors = fs.get(filename)
         if success and data:
-            logging.warn(data)
             key = build_s3_constants_key(filename)
-            s3.put(key, data)
-        
+            s3.put(key, data)        
     return {'files':constants_files}, True, []

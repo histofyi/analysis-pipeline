b2b60f81ce27f27945a87a5320606206103c9d7a
Chris Thorpe <33100149+drchristhorpe@users.noreply.github.com>
2022-01-15 17:33:43 +0000

    Added class i starts to constants

--- constants_pipeline/pipeline_actions/upload_constants_block.py
+++ constants_pipeline/pipeline_actions/upload_constants_block.py
@@ -7,12 +7,13 @@ import logging
 
 fs = filesystemProvider('constants_pipeline/files')
 
-constants_files = ['amino_acids','chains','hetatoms','loci','peptide_lengths','species_overrides','species']
+constants_files = ['amino_acids','chains','class_i_starts','hetatoms','loci','peptide_lengths','species_overrides','species']
 
 
 def upload_constants(aws_config):
     s3 = s3Provider(aws_config)
     for filename in constants_files:
+        print(filename)
         data, success, errors = fs.get(filename)
         if success and data:
             key = build_s3_constants_key(filename)

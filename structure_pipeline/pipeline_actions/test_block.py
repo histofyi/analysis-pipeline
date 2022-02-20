8a94ef21113a93f6539356cf64e5ee3c62b7d5c6
Chris Thorpe <33100149+drchristhorpe@users.noreply.github.com>
2022-02-20 21:06:39 +0000

    Added the force argument to the test function

--- structure_pipeline/pipeline_actions/test_block.py
+++ structure_pipeline/pipeline_actions/test_block.py
@@ -4,17 +4,18 @@ from common.providers import s3Provider, awsKeyProvider
 
 import logging
 
-def test(pdb_code:str, aws_config: Dict) -> Tuple[Dict, bool, Union[List, None]]:
+def test(pdb_code:str, aws_config: Dict, force: bool=False) -> Tuple[Dict, bool, Union[List, None]]:
     """
     This function does nothing really, it echoes back the pdb_code in a dictionary. It's role is to act as a small test function for the pipeline
     
     Args:
         pdb_code (str): the pdb code of a structure
         aws_config (Dict): the configuration details for AWS for the current app
+        force (bool): whether the data in this step should be overwritten. 
 
     Returns:
         Dict: A dictionary of generated data (data)
         bool: A boolean of True or False (success)
         List: A list of error strings (errors)
     """
-    return {'pdb_code':pdb_code}, True, None
+    return {'pdb_code':pdb_code, 'force': force}, True, None

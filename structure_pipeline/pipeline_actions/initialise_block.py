7199c943032189f1c820e0996e3e59e02df939ac
Chris Thorpe <33100149+drchristhorpe@users.noreply.github.com>
2022-02-20 21:07:47 +0000

    Updated the initialise block to use AWS and to not change the data unless the force argument is provided as True

--- structure_pipeline/pipeline_actions/initialise_block.py
+++ structure_pipeline/pipeline_actions/initialise_block.py
@@ -1,7 +1,19 @@
+from typing import Dict, Tuple, List, Optional, Union
+
 from common.providers import s3Provider, awsKeyProvider
 
+import json
 
-def build_core(pdb_code):
+def build_core(pdb_code: str) -> Dict:
+    """
+    This function returns the core metadata dictionary which is filled out by the different methods on the structure pipeline
+
+    Args:
+        pdb_code (str): the pdb code of the structure
+
+    Returns:
+        Dict: the default prototype data dictionary for structure metadata
+    """
     return {
         'pdb_code':pdb_code,
         'organism_common':None,
@@ -19,15 +31,34 @@ def build_core(pdb_code):
         'missing_residues':[],
         'complex_count':0,
         'chain_count':0,
-        'title':0,
+        'title':None,
         'authors':[],
         'publication':{}
     }
 
 
-def initialise(pdb_code, aws_config):
+def initialise(pdb_code: str, aws_config: Dict, force:bool=False) -> Tuple[Dict, bool, Union[List, None]]:
+    """
+    This function initialises the records for a structure. 
+    
+    If the force parameter is set to True it resets the record to the empty default dictionary. 
+    
+    This SHOULD only be used to reset the record if there is a concern about the metadata integrity in the pipeline. 
+    
+    Args:
+        pdb_code (str): the pdb code of the structure
+        aws_config (Dict): the configuration details for AWS for the current app
+        force (bool): determines whether the metadata on a particular structure should be reset
+
+    Returns:
+        Dict: A dictionary of generated data (data)
+        bool: A boolean of True or False (success)
+        List: A list of error strings (errors)
+    """
     s3 = s3Provider(aws_config)
-    key = awsKeyProvider.block_key(pdb_code, 'core', 'info')
-    payload = build_core(pdb_code)
-    s3.put(key, payload)
-    return payload, True, None
+    key = awsKeyProvider().block_key(pdb_code, 'core', 'info')
+    data, success, errors = s3.get(key)
+    if not data or force is True:
+        data, success, errors = s3.put(key, build_core(pdb_code))
+        data = json.loads(data)
+    return data, success, errors

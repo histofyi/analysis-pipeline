27242c6ad74d2b47a3ba659d6903342580967379
Chris Thorpe <33100149+drchristhorpe@users.noreply.github.com>
2022-02-20 19:34:30 +0000

    Added type hints and docstring comments

--- structure_pipeline/pipeline_actions/test_block.py
+++ structure_pipeline/pipeline_actions/test_block.py
@@ -1,7 +1,20 @@
+from typing import Dict, Tuple, List, Optional, Union
+
 from common.providers import s3Provider, awsKeyProvider
 
 import logging
 
-def test(pdb_code, aws_config):
-    logging.warn(pdb_code)
+def test(pdb_code:str, aws_config: Dict) -> Tuple[Dict, bool, Union[List, None]]:
+    """
+    This function does nothing really, it echoes back the pdb_code in a dictionary. It's role is to act as a small test function for the pipeline
+    
+    Args:
+        pdb_code (str): the pdb code of a structure
+        aws_config (Dict): the configuration details for AWS for the current app
+
+    Returns:
+        Dict: A dictionary of generated data (data)
+        bool: A boolean of True or False (success)
+        List: A list of error strings (errors)
+    """
     return {'pdb_code':pdb_code}, True, None

fd8afb748f0388372debbab8d583c1ac62eeccfb
Chris Thorpe <33100149+drchristhorpe@users.noreply.github.com>
2022-02-23 14:38:51 +0000

    Added match_chains back into pipeline

--- structure_pipeline/pipeline_actions/__init__.py
+++ structure_pipeline/pipeline_actions/__init__.py
@@ -4,4 +4,4 @@ from .fetch_structure_block import get_pdb_structure
 from .structure_info_block import parse_pdb_header
 from .rcsb_info_block import fetch_rcsb_info
 from .alike_chains_block import alike_chains
-#from .match_chains_block import match_chains
+from .match_chains_block import match_chains

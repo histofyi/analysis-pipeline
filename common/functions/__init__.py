da059032c627d65cf80b1f9f07814bade7ccb350
Chris Thorpe <33100149+drchristhorpe@users.noreply.github.com>
2022-03-27 13:29:03 +0100

    Updated and improved the slugify function

--- common/functions/__init__.py
+++ common/functions/__init__.py
@@ -6,7 +6,12 @@ from datetime import datetime
 import logging
 
 def slugify(string):
-    return string.replace(' ','_').lower()
+    slug_char = '_'
+    to_replace = [' ','-','.',',','[',']','{','}','(',')','/','\\','|']
+    for replace_me in to_replace:
+        if replace_me in string:
+            string = string.replace(replace_me, slug_char)
+    return string.lower()
     
 
 def de_slugify(slug):

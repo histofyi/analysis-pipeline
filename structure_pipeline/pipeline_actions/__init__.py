82a0b45d8093ba1a85acf1b4496fa47d70ffe5bf
Chris Thorpe <33100149+drchristhorpe@users.noreply.github.com>
2022-04-24 16:07:47 +0100

    Removed unused functions, added some comments

--- structure_pipeline/pipeline_actions/__init__.py
+++ structure_pipeline/pipeline_actions/__init__.py
@@ -1,17 +1,17 @@
 from .test_block import test
 from .view_block import view
-from .initialise_block import initialise
+from .initialise_block import initialise # can reset all of the data in the core file #TODO consider how to reset all data for a pdb_code
 
-from .summary_info_block import fetch_summary_info
-from .publication_info_block import fetch_publication_info
-from .experiment_info_block import fetch_experiment_info
+from .summary_info_block import fetch_summary_info # fetch from PDBe REST API
+from .publication_info_block import fetch_publication_info # fetch from PDBe REST API
+from .experiment_info_block import fetch_experiment_info # fetch from PDBe REST API
 
-from .fetch_structure_block import get_pdbe_structures
-from .assign_chains_block import alike_chains, assign_chains
-from .match_chains_block import match_chains
-from .match_peptide_block import api_match_peptide
+from .fetch_structure_block import get_pdbe_structures # fetch from PDBe Coordinate Server
+from .assign_chains_block import alike_chains, assign_chains # computed
+from .match_chains_block import match_chains # computed from IPD data using Levenshtein distance where needed
+from .match_peptide_block import api_match_peptide # computed from IEDB REST API
 
-from .align_structures_block import align_structures
+from .align_structures_block import align_structures # computed using BioPython
 
 
 

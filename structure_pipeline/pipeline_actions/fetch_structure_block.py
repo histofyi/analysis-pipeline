64956b54974e5c1945d7969d213ed927a72d4316
Chris Thorpe <33100149+drchristhorpe@users.noreply.github.com>
2022-04-24 15:05:57 +0100

    Bug fixes
    #TODO fix issue where initialisation has happened but files have been downloaded

--- structure_pipeline/pipeline_actions/fetch_structure_block.py
+++ structure_pipeline/pipeline_actions/fetch_structure_block.py
@@ -3,7 +3,7 @@ from typing import Dict, List, Tuple
 from datetime import datetime
 
 from common.providers import s3Provider, httpProvider, awsKeyProvider
-from common.helpers import fetch_core, update_block
+from common.helpers import fetch_core, update_block, process_step_errors
 
 import logging
 
@@ -14,17 +14,21 @@ def download_cif_file(pdb_code, assembly_id):
 
 
 def get_pdbe_structures(pdb_code:str, aws_config: Dict, force:bool=False):
+    step_errors = []
     core, success, errors = fetch_core(pdb_code, aws_config)
+    if errors:
+        step_errors.append(errors)
     s3 = s3Provider(aws_config)
     action = {'assemblies':{'files':{}}}
-
     has_updates = False
     if core['assembly_count'] is not None:
         assembly_id = 1
         while assembly_id <= core['assembly_count']:
             assembly_identifier = f'{pdb_code}_{assembly_id}'
-            key = awsKeyProvider().cif_file_key(pdb_code, assembly_identifier, 'split')
+            key = awsKeyProvider().cif_file_key(assembly_id, 'split')
             cif_data, success, errors = s3.get(key, data_format='cif')
+            if errors:
+                step_errors.append(errors)
             if not success:
                 has_updates = True
                 cif_data = download_cif_file(pdb_code, assembly_id)
@@ -36,58 +40,21 @@ def get_pdbe_structures(pdb_code:str, aws_config: Dict, force:bool=False):
                     }
                 }
             else:
-                action['assemblies']['files'][str(assembly_id)] = core['assemblies']['files'][str(assembly_id)]
-                cif_data = cif_data.decode('utf-8')
+                if 'files' in core['assemblies']:
+                    action['assemblies']['files'][str(assembly_id)] = core['assemblies']['files'][str(assembly_id)]
+                else:
+                    # TODO fix the edge case where we already have the files but have initialsed the records
+                    logging.warn("EDGE CASE TO FIX!")
             assembly_id += 1
         if has_updates:
             update = action
             data, success, errors = update_block(pdb_code, 'core', 'info', update, aws_config)
+            if errors:
+                step_errors.append(errors)
             core = data
     output = {
         'action': action,
         'core': core
     }
-    return output, success, errors
-
-
-
-
-# OLD PDB METHODS
-
-def download_pdb_file(pdb_code:str) -> str:
-    """
-    This function downloads the PDB file specified
-
-    Args:
-        pdb_code (str): the code of the PDB file to be downloaded
-
-    Returns:
-        str : the PDB file
-
-    """
-    url = f'https://files.rcsb.org/download/{pdb_code}.pdb'
-    pdb_data = httpProvider().get(url, 'txt')
-    return pdb_data
-
-
-def get_pdb_structure(pdb_code:str, aws_config: Dict, force:bool=False) -> Tuple[str, bool, List]:
-    """
-    This function retrieves a PDB file from S3. If the file is not already in S3 it will retrieve it from the RCSB and persist it to S3
-
-    Args:
-        pdb_code (str): the code of the PDB file
-        aws_config (Dict): the AWS configuration for the environment
-        force (bool): not currently used, may be implemented to force a re-download in the case of a revised structure
-    """
-    key = awsKeyProvider().structure_key(pdb_code, 'raw')
-    s3 = s3Provider(aws_config)
-    pdb_data, success, errors = s3.get(key, data_format='pdb')
-    if not success:
-        pdb_data = download_pdb_file(pdb_code)
-        if pdb_data:
-            s3.put(key, pdb_data, data_format='pdb')
-        return pdb_data, True, None
-    else:
-        return pdb_data.decode('utf-8'), True, None
-
+    return output, success, process_step_errors(step_errors)
 

1dc14b733df797bca6738655868f3a20ce2ed3c1
Chris Thorpe <33100149+drchristhorpe@users.noreply.github.com>
2022-04-29 13:13:30 +0100

    Renaming files for consistency within the structure pipeline

--- structure_pipeline/pipeline_actions/__init__.py
+++ structure_pipeline/pipeline_actions/__init__.py
@@ -1,21 +1,20 @@
-from .test_block import test
-from .view_block import view
-from .initialise_block import initialise # can reset all of the data in the core file #TODO consider how to reset all data for a pdb_code
+from .test import test
+from .view import view
+from .initialise import initialise # can reset all of the data in the core file #TODO consider how to reset all data for a pdb_code
 
-from .summary_info_block import fetch_summary_info # fetch from PDBe REST API
-from .publication_info_block import fetch_publication_info # fetch from PDBe REST API
-from .experiment_info_block import fetch_experiment_info # fetch from PDBe REST API
+from .summary_info import fetch_summary_info # fetch from PDBe REST API
+from .publication_info import fetch_publication_info # fetch from PDBe REST API
+from .experiment_info import fetch_experiment_info # fetch from PDBe REST API
 
-from .fetch_structure_block import get_pdbe_structures # fetch from PDBe Coordinate Server
-from .assign_chains_block import assign_chains # computed
-from .match_chains_block import match_chains # computed from IPD data using Levenshtein distance where needed
-from .match_peptide_block import api_match_peptide # computed from IEDB REST API
+from .fetch_structure import get_pdbe_structures # fetch from PDBe Coordinate Server
+from .assign_chains import assign_chains # computed
+from .match_chains import match_chains # computed from IPD data using Levenshtein distance where needed
+from .match_peptide import api_match_peptide # computed from IEDB REST API
 
-from .align_structures_block import align_structures # computed with BioPython
-from .peptide_neighbours_block import peptide_neighbours  # computed using BioPython
-from .peptide_features_block import peptide_features  # computed from peptide_neighbours
-
-from .extract_peptides_block import extract_peptides  # computed with BioPython
+from .align_structures import align_structures # computed with BioPython
+from .peptide_neighbours import peptide_neighbours  # computed using BioPython
+from .peptide_features import peptide_features  # computed from peptide_neighbours
+from .extract_peptides import extract_peptides  # computed with BioPython
 from .measure_peptide_angles import measure_peptide_angles  # computed with BioPython
 from .measure_cleft_angles import measure_cleft_angles  # computed with BioPython
 from .measure_distances import measure_distances  # computed with BioPython

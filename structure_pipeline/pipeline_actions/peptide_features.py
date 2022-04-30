e826f16044ed4f810d4f0a734be05bef9a864b65
Chris Thorpe <33100149+drchristhorpe@users.noreply.github.com>
2022-04-30 21:52:07 +0100

    Added in additional clauses for non-peptide bearing molecules

--- structure_pipeline/pipeline_actions/peptide_features.py
+++ structure_pipeline/pipeline_actions/peptide_features.py
@@ -9,64 +9,74 @@ import logging
 
 
 def peptide_features(pdb_code:str, aws_config:Dict, force:bool=False) -> Tuple[Dict,bool,List]:
+    step_errors = []
     core, success, errors = fetch_core(pdb_code, aws_config)
     s3 = s3Provider(aws_config)
     peptide_key = awsKeyProvider().block_key(pdb_code, 'peptide_neighbours', 'info')
     sorted_peptide, success, errors = s3.get(peptide_key)
     
+    logging.warn(sorted_peptide)
+    
     extended_peptide = False
     exposed_bulge = False
     peptide_features = {}
     extensions = None
+    if sorted_peptide is not None:
+        for assembly_id in sorted_peptide:
+            peptide_features[assembly_id] = {
+                'PN':None,
+                'PC':None,
+                'exposed':[],
+                'buried':[]
+            }
+            members = [pdb_code]
+            for position in sorted_peptide[assembly_id]:
+                abd_contacts = [neighbour['position'] for neighbour in sorted_peptide[assembly_id][position]['neighbours']]
+                if len(abd_contacts) <=3:
+                    peptide_features[assembly_id]['exposed'].append(position)
+                else:
+                    peptide_features[assembly_id]['buried'].append(position)
+                if 7 in abd_contacts and 171 in abd_contacts:
+                    peptide_features[assembly_id]['PN'] = position
+                    
+                if 116 in abd_contacts and 143 in abd_contacts:
+                    peptide_features[assembly_id]['PC'] = position
 
-    for assembly_id in sorted_peptide:
-        peptide_features[assembly_id] = {
-            'PN':None,
-            'PC':None,
-            'exposed':[],
-            'buried':[]
-        }
-        members = [pdb_code]
-        for position in sorted_peptide[assembly_id]:
-            abd_contacts = [neighbour['position'] for neighbour in sorted_peptide[assembly_id][position]['neighbours']]
-            if len(abd_contacts) <=3:
-                peptide_features[assembly_id]['exposed'].append(position)
+            if peptide_features[assembly_id]['PN'] is not None:
+                if int(peptide_features[assembly_id]['PN']) > 1:
+                    extended_peptide = True 
+                    extensions = {'n-terminal':{'positions':[]}}
+                    set_title = f'N-terminally extended structures'
+                    set_slug = slugify('c terminally extended')
+                    set_description = f'Structures containing a N-terminal extension'
+                    context = 'features'
+                    itemset, success, errors = itemSet(set_slug, context).create_or_update(set_title, set_description, members, context)
             else:
-                peptide_features[assembly_id]['buried'].append(position)
-            if 7 in abd_contacts and 171 in abd_contacts:
-                peptide_features[assembly_id]['PN'] = position
-                
-            if 116 in abd_contacts and 143 in abd_contacts:
-                peptide_features[assembly_id]['PC'] = position
+                step_errors.append('no_pn_in_peptide')
 
-        if int(peptide_features[assembly_id]['PN']) > 1:
-            extended_peptide = True 
-            extensions = {'n-terminal':{'positions':[]}}
-            set_title = f'N-terminally extended structures'
-            set_slug = slugify('c terminally extended')
-            set_description = f'Structures containing a N-terminal extension'
-            context = 'features'
-            itemset, success, errors = itemSet(set_slug, context).create_or_update(set_title, set_description, members, context)
+            if peptide_features[assembly_id]['PC'] is not None:
+                if int(peptide_features[assembly_id]['PC']) < len(sorted_peptide[assembly_id]): 
+                    extended_peptide = True
+                    extensions = {'c-terminal':{'positions':[]}}
+                    set_title = f'C-terminally extended structures'
+                    set_slug = slugify('c terminally extended')
+                    set_description = f'Structures containing a C-terminal extension'
+                    context = 'features'
+                    itemset, success, errors = itemSet(set_slug, context).create_or_update(set_title, set_description, members, context)
+            else:
+                step_errors.append('no_pc_in_peptide')
 
-        if int(peptide_features[assembly_id]['PC']) < len(sorted_peptide[assembly_id]): 
-            extended_peptide = True
-            extensions = {'c-terminal':{'positions':[]}}
-            set_title = f'C-terminally extended structures'
-            set_slug = slugify('c terminally extended')
-            set_description = f'Structures containing a C-terminal extension'
-            context = 'features'
-            itemset, success, errors = itemSet(set_slug, context).create_or_update(set_title, set_description, members, context)
+            if extended_peptide:
+                set_title = f'Extended peptide structures'
+                set_slug = slugify('extended')
+                set_description = f'Structures containing an extension at either end of the cleft'
+                context = 'features'
+                itemset, success, errors = itemSet(set_slug, context).create_or_update(set_title, set_description, members, context)
 
-        if extended_peptide:
-            set_title = f'Extended peptide structures'
-            set_slug = slugify('extended')
-            set_description = f'Structures containing an extension at either end of the cleft'
-            context = 'features'
-            itemset, success, errors = itemSet(set_slug, context).create_or_update(set_title, set_description, members, context)
-
-    peptide_features['extended_peptide'] = extended_peptide
-    peptide_features['extensions'] = extensions
-    step_errors = []
+        peptide_features['extended_peptide'] = extended_peptide
+        peptide_features['extensions'] = extensions
+    else:
+        step_errors.append('no_sorted_peptide')
     action = {
         'peptide_features':peptide_features
     }
--- structure_pipeline/pipeline_actions/peptide_neighbours.py
+++ structure_pipeline/pipeline_actions/peptide_neighbours.py
@@ -9,6 +9,7 @@ import logging
 
 
 def peptide_neighbours(pdb_code:str, aws_config:Dict, force:bool=False) -> Tuple[Dict,bool,List]:
+    logging.warn(pdb_code)
     step_errors = []
     core, success, errors = fetch_core(pdb_code, aws_config)
     s3 = s3Provider(aws_config)
@@ -26,64 +27,69 @@ def peptide_neighbours(pdb_code:str, aws_config:Dict, force:bool=False) -> Tuple
                 peptide_chains = chains[chain]['chains']
             elif chains[chain]['best_match'] == 'class_i_alpha':
                 mhc_chains = chains[chain]['chains']
-        chains_to_test = peptide_chains + mhc_chains
-        for assembly_id in core['assemblies']['files']:
-            peptide_neighbours[assembly_id] = {}
-            cif_key = core['assemblies']['files'][assembly_id]['files']['file_key']
-            assembly_identifier = f'{pdb_code}_{assembly_id}'
-            structure = load_cif(cif_key, assembly_identifier, aws_config)
-            if structure:
-                all_atoms = []
-                for chain in structure.get_chains():
-                    if chain.get_id() in chains_to_test:
-                        if chain.get_id() in peptide_chains:
-                            class_i_peptide = chain.get_id()
-                        else:
-                            class_i_alpha = chain.get_id()
-                        contacts[chain.get_id()] = {}
-                        for residue in chain:
-                            if residue.id[0] == ' ':
-                                for atom in residue:
-                                    all_atoms.append(atom)
-                neighbor = Bio.PDB.NeighborSearch(all_atoms)
-                neighbours = neighbor.search_all(5, level='R')
-
-                for residue_pair in neighbours:
-                    residue_1 = residue_pair[0]
-                    residue_2 = residue_pair[1]
-                    if residue_1.get_parent().id != residue_2.get_parent().id:
-                        chain_pair = [residue_1.get_parent().id, residue_2.get_parent().id]
-                        if class_i_peptide in chain_pair and class_i_alpha in chain_pair:
-                            if residue_1.get_parent().id == class_i_alpha:
-                                class_i_details = {'residue':residue_1.resname, 'position':residue_1.get_id()[1]}
-                                peptide_details = {'residue':residue_2.resname, 'position':residue_2.get_id()[1]}
+        if len(peptide_chains) > 0:
+            chains_to_test = peptide_chains + mhc_chains
+            for assembly_id in core['assemblies']['files']:
+                peptide_neighbours[assembly_id] = {}
+                cif_key = core['assemblies']['files'][assembly_id]['files']['file_key']
+                assembly_identifier = f'{pdb_code}_{assembly_id}'
+                structure = load_cif(cif_key, assembly_identifier, aws_config)
+                if structure:
+                    all_atoms = []
+                    for chain in structure.get_chains():
+                        if chain.get_id() in chains_to_test:
+                            if chain.get_id() in peptide_chains:
+                                class_i_peptide = chain.get_id()
                             else:
-                                peptide_details = {'residue':residue_1.resname, 'position':residue_1.get_id()[1]}
-                                class_i_details = {'residue':residue_2.resname, 'position':residue_2.get_id()[1]}
+                                class_i_alpha = chain.get_id()
+                            contacts[chain.get_id()] = {}
+                            for residue in chain:
+                                if residue.id[0] == ' ':
+                                    for atom in residue:
+                                        all_atoms.append(atom)
+                    neighbor = Bio.PDB.NeighborSearch(all_atoms)
+                    neighbours = neighbor.search_all(5, level='R')
+                    for residue_pair in neighbours:
+                        residue_1 = residue_pair[0]
+                        residue_2 = residue_pair[1]
+                        if residue_1.get_parent().id != residue_2.get_parent().id:
+                            chain_pair = [residue_1.get_parent().id, residue_2.get_parent().id]
+                            if class_i_peptide in chain_pair and class_i_alpha in chain_pair:
+                                if residue_1.get_parent().id == class_i_alpha:
+                                    class_i_details = {'residue':residue_1.resname, 'position':residue_1.get_id()[1]}
+                                    peptide_details = {'residue':residue_2.resname, 'position':residue_2.get_id()[1]}
+                                else:
+                                    peptide_details = {'residue':residue_1.resname, 'position':residue_1.get_id()[1]}
+                                    class_i_details = {'residue':residue_2.resname, 'position':residue_2.get_id()[1]}
 
-                        #TODO offset the peptide and MHC residue ids if needed
+                            #TODO offset the peptide and MHC residue ids if needed
 
-                        class_i_residue_id = class_i_details['position']
-                        peptide_residue_id = peptide_details['position']
+                            class_i_residue_id = class_i_details['position']
+                            peptide_residue_id = peptide_details['position']
 
 
-                        if class_i_residue_id not in contacts[class_i_alpha]:
-                            contacts[class_i_alpha][class_i_residue_id] = {'position':class_i_residue_id, 'residue':class_i_details['residue'], 'neighbours':[] }
-                        contacts[class_i_alpha][class_i_residue_id]['neighbours'].append(peptide_details)
+                            if class_i_residue_id not in contacts[class_i_alpha]:
+                                contacts[class_i_alpha][class_i_residue_id] = {'position':class_i_residue_id, 'residue':class_i_details['residue'], 'neighbours':[] }
+                            contacts[class_i_alpha][class_i_residue_id]['neighbours'].append(peptide_details)
 
-                        if peptide_residue_id not in contacts[class_i_peptide]:
-                            contacts[class_i_peptide][peptide_residue_id] = {'position':peptide_residue_id, 'residue':peptide_details['residue'], 'neighbours':[] }
-                        contacts[class_i_peptide][peptide_residue_id]['neighbours'].append(class_i_details)
+                            if peptide_residue_id not in contacts[class_i_peptide]:
+                                contacts[class_i_peptide][peptide_residue_id] = {'position':peptide_residue_id, 'residue':peptide_details['residue'], 'neighbours':[] }
+                            contacts[class_i_peptide][peptide_residue_id]['neighbours'].append(class_i_details)
 
-                sorted_peptide = dict(sorted(contacts[class_i_peptide].items()))
-            sorted_peptides[assembly_id] = sorted_peptide
-        peptide_key = awsKeyProvider().block_key(pdb_code, 'peptide_neighbours', 'info')
-        s3.put(peptide_key, sorted_peptides)
+                    sorted_peptide = dict(sorted(contacts[class_i_peptide].items()))
+                sorted_peptides[assembly_id] = sorted_peptide
+            peptide_key = awsKeyProvider().block_key(pdb_code, 'peptide_neighbours', 'info')
+            s3.put(peptide_key, sorted_peptides)
+        else:
+            logging.warn('NO PEPTIDE')
+            step_errors.append('no_peptide')
+    else:
+        logging.warn('NOT CLASS I')
+        step_errors.append('not_class_i')
     # TODO Class II 
-    #elif mhc_class == 'class_ii':
     action = {'peptide_neighbours':sorted_peptides}
     output = {
         'action':action,
         'core':core
     }
-    return output, True, []
+    return output, True, step_errors

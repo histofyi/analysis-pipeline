4bcbe575a264947374d34dd5f0c60e16e84233a6
Chris Thorpe <33100149+drchristhorpe@users.noreply.github.com>
2022-06-04 06:57:46 +0100

    Initial view for features

--- features_pipeline/__init__.py
+++ features_pipeline/__init__.py
@@ -23,14 +23,32 @@ def features_home_handler(userobj):
 @features_views.get('/view/<string:pdb_code>')
 @check_user
 @requires_privilege('users')
-#@templated('features/view')
+@templated('features/view')
 def features_view_handler(userobj, pdb_code):
     s3 = s3Provider(current_app.config['AWS_CONFIG'])
     core, success, errors = fetch_core(pdb_code, current_app.config['AWS_CONFIG'])
     if success:
         block_key = awsKeyProvider().block_key(pdb_code, 'features', 'structures')
-        print (block_key)
         features, success, errors = s3.get(block_key)
     else:
+        features = None
+        core = None
         errors = ['not_mhc']
-    return {'userobj': userobj, 'pdb_code': pdb_code, 'features':features, 'success': success, 'errors':errors}
+    return {'userobj': userobj, 'pdb_code': pdb_code, 'features':features, 'success': success, 'errors':errors, 'structure':core}
+
+
+@features_views.get('/add/<string:pdb_code>')
+@check_user
+@requires_privilege('users')
+#@templated('features/add')
+def features_add_handler(userobj, pdb_code):
+    s3 = s3Provider(current_app.config['AWS_CONFIG'])
+    core, success, errors = fetch_core(pdb_code, current_app.config['AWS_CONFIG'])
+    if success:
+        block_key = awsKeyProvider().block_key(pdb_code, 'features', 'structures')
+        features, success, errors = s3.get(block_key)
+    else:
+        features = None
+        core = None
+        errors = ['not_mhc']
+    return {'userobj': userobj, 'pdb_code': pdb_code, 'features':features, 'success': success, 'errors':errors, 'structure':core}

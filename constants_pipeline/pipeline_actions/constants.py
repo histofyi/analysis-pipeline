1202ae9b2ffc573230c18bc1854611f95e782552
Chris Thorpe <33100149+drchristhorpe@users.noreply.github.com>
2022-06-15 12:07:56 +0100

    Migrated the class I starts to be part of a pan MHC set of start sequences, class I , class II, beta2m

--- constants_pipeline/pipeline_actions/constants.py
+++ constants_pipeline/pipeline_actions/constants.py
@@ -4,7 +4,7 @@ from typing import Dict, List
 CONSTANTS_FILES = {
     'amino_acids':{'slug':'amino_acids','name':'Amino acids','description':'A set of mappings between one letter and three letter codes.'},
     'chains':{'slug':'chains','name':'Chains','description':'A set of standardised names, colours and details of individual chains of immune system complexes.'},
-    'class_i_starts':{'slug':'class_i_starts','name':'Class I alpha 1 starts', 'description':'A set of the first four amino acids of the alpha 1 chain of Class I molecules to allow for removal of signal peptide sequences.'},
+    'mhc_starts':{'slug':'mhc_starts','name':'MHC chain starts', 'description':'A set of the first amino acids of the different MHC chains to allow for removal of signal peptide sequences.'},
     'hetatoms':{'slug':'hetatoms', 'name':'Hetatoms', 'description': 'A set of the HETATOMS found in MHC structures in the PDB'},
     'loci':{'slug':'loci', 'name': 'Loci', 'description':'A set of MHC Class I and Class II loci derived from the IPD dataset'},
     'peptide_lengths':{'slug':'peptide_lengths', 'name': 'Peptide lengths', 'description': 'A set of human readable names for different length peptides' },
